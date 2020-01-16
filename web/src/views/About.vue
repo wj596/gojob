478c17cfb2342e427b1ca0604fa21aa27664ed1f
wangjie <wangjie>
2020-01-17 00:32:49 +0800

    1.0.0 Bate

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -2,7 +2,13 @@
 
 <div>
 
-关于本系统
+项目名称：gojob分布式任务调度系统 <br><br>
+版本：   1.0.0 Bate <br><br>
+github： <a href="https://github.com/wj596/gojob" target="_blank"> https://github.com/wj596/gojob</a> <br><br>
+使用说明：<a href="https://github.com/wj596/gojob/wiki" target="_blank"> https://github.com/wj596/gojob/wiki</a> <br><br>
+作者博客：<a href="https://www.jianshu.com/u/ffa3cba4c604" target="_blank"> https://www.jianshu.com/u/ffa3cba4c604</a> <br><br>
+
+有意见或者建议可以在github上提交issues <br><br>
 
 </div>
 
